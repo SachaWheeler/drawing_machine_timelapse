66ba46f028cc1596ed9e0a2acebff1e3e718b309
SachaWheeler <sacha@sachawheeler.com>
2022-11-08 14:15:06 +0000

    clean up dir

--- capture.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -r 15 -b 10240k -f image2 -i north-%d.jpg a.mp4
--- opencv/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-path = 'images/timelapse/'
-for filename in os.listdir(path):
-    num, suffic = filename.split('.')
-    num = num.zfill(4)
-    new_filename = num + ".jpg"
-    # print(filename, new_filename)
-    os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
